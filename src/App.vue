e46575fea056fa733a6339fc31321bbd7f8633b7
Alexander Hardman <alexander.hardman@gmail.com>
2022-10-25 12:18:02 -0400

    last minute scaffold updates

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <v-app> <v-main> </v-main></v-app>
+  <v-app>
+    <v-main v-if="isLaunchDarklyReady">
+      <v-app-bar v-if="isLaunchDarklyReady" :elevation="2">
+        <v-app-bar-title>Features Powered by LaunchDarkly</v-app-bar-title>
+        <v-spacer></v-spacer>
+
+        <ThemeSwitcher />
+        <v-spacer></v-spacer>
+        <Login />
+      </v-app-bar>
+      <v-navigation-drawer
+        permanent
+        width="325"
+        style="margin-left: 20px; margin-top: 25px"
+      >
+        <QRCode />
+      </v-navigation-drawer>
+      <TimeLine />
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-export default {
-  name: "App",
+<script setup>
+import { ref } from "vue";
+import { useLDReady, useLDClient } from "launchdarkly-vue-client-sdk";
+import QRCode from "./components/QRCode.vue";
+import TimeLine from "./components/Time-Line.vue";
+import ThemeSwitcher from "./components/ThemeSwitcher.vue";
+import Login from "./components/Login.vue";
 
-  components: {},
+let isLaunchDarklyReady = ref(false);
+try {
+  isLaunchDarklyReady = useLDReady();
+  console.log("is launch darkly ready", isLaunchDarklyReady);
 
-  data: () => ({
-    //
-  }),
-};
+  const client = useLDClient();
+
+  client.identify({
+    key: "anonymous",
+    custom: {
+      // Role:
+    },
+  });
+} catch (error) {
+  console.error("error checking LD Ready", error);
+}
 </script>
